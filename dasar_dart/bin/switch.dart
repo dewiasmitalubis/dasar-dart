12b002d2693f972f7c63beeea84f487178b610d3
Dewiasmitalubis <mytaloebis@gmail.com>
2021-03-16 16:06:45 +0700

    Switch

---
+++ dasar_dart/bin/switch.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+void main(List<String> arguments) {
+  int number = int.tryParse(stdin.readLineSync());
+
+  switch(number){
+    case 0:
+    print("nol");
+    break;
+
+    case 1:
+    print(number);
+    print("satu");
+    break;
+
+    case 2:
+    print("dua");
+    break;
+
+    default:
+    print("bilangan lain");
+  }
+
+
+}
