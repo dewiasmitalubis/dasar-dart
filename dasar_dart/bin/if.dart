250743100883ebed730ce4f614041b247fd9613d
Dewiasmitalubis <mytaloebis@gmail.com>
2021-03-17 00:11:18 +0700

    Praktikum minggu 1

---
+++ dasar_dart/bin/if.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main(List<String> arguments) {
+  int number = int.tryParse(stdin.readLineSync());
+
+   if(number > 0){
+     print("positif");
+  }
+  else if(number < 0){
+    print("negatif");
+  }
+  else{
+    print("nol");
+  }
+} 
+ 
